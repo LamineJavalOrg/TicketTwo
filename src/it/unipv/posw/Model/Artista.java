6d6f3ab396293e5465d3e8a8144b8a53cc867a71
Mirko <mirkohtcz@gmail.com>
2026-05-29 11:05:41 +0200

    Creazioni classi base Carrello, Biglietto, RiepilogoAcquisti e Artista (Costruzione)

---
+++ src/it/unipv/posw/Model/Artista.java
@@ -0,0 +1,23 @@
+package it.unipv.posw.Model;
+
+public class Artista {
+	
+	private int id_artista;
+	private String nome_darte;
+	/**
+	 * @author rkomi-dev
+	 *
+	 */
+	public Artista(int id_artista, String nome_darte) {
+		super();
+		this.id_artista = id_artista;
+		this.nome_darte = nome_darte;
+	}
+	public int getId_artista() {
+		return id_artista;
+	}
+	public String getNome_darte() {
+		return nome_darte;
+	}
+
+}
---
+++ src/it/unipv/posw/Model/Carrello.java
@@ -0,0 +1,47 @@
+package it.unipv.posw.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrello {
+	
+	private static Carrello instance;
+    private List<Biglietto> items;
+    
+    /**
+	 * @author rkomi-dev
+	 *
+	 */
+    
+    private Carrello() {
+        this.items = new ArrayList<>();
+    }
+
+    public static Carrello getInstance() {
+        if (instance == null) {
+            instance = new Carrello();
+        }
+        return instance;
+    }
+
+    public void aggiungi(Biglietto b) { 
+    	items.add(b); 
+    }
+    
+    public List<Biglietto> getItems() { 
+    	return items; 
+    }
+    
+    public void svuota() { 
+    	items.clear(); 
+    }
+    
+    public double getTotale() {
+        double totale = 0;
+        for (Biglietto b : items) {
+            totale += b.getPrezzo(); 
+        }
+        return totale;
+    }
+
+}
