09ce803d40e3b880f372eab84361e8f214724262
Mirko <mirkohtcz@gmail.com>
2026-05-29 10:44:08 +0200

    Creazione classe Cliente e aggiustamento .gitignore

---
+++ src/it/unipv/posw/Model/Cliente.java
@@ -0,0 +1,16 @@
+package it.unipv.posw.Model;
+
+import java.time.LocalDate;
+
+public class Cliente extends Utente {
+	
+	/**
+	 * @author rkomi-dev
+	 */
+
+	public Cliente(String nome, String cognome, LocalDate data_nascita, String email, String password) {
+		super(nome, cognome, data_nascita, email, password);
+		// TODO Auto-generated constructor stub
+	}
+
+}
