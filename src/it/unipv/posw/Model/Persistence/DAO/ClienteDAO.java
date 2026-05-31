d5c63c38e20737fe30829ddd2208b9a143cbaef1
Mirko <mirkohtcz@gmail.com>
2026-05-31 16:01:37 +0200

    Creazione classe ClienteDAO e relativa interfaccia (Costruzione)

---
+++ src/it/unipv/posw/Model/Persistence/DAO/ClienteDAO.java
@@ -0,0 +1,52 @@
+package it.unipv.posw.Model.Persistence.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import it.unipv.posw.Model.Cliente;
+import it.unipv.posw.Model.Persistence.DBConnection;
+import it.unipv.posw.Model.Persistence.DAO.Interface.IClienteDAO;
+
+/**
+ * @author rkomi-dev
+ */
+
+public class ClienteDAO implements IClienteDAO {
+	
+	private Connection c;
+	
+	public ClienteDAO() {
+		super();
+	}
+
+	@Override
+	public boolean salvaCliente(Cliente cliente) {
+		PreparedStatement ps;
+        String query = "INSERT INTO Utente (nome, cognome, email, password, data_nascita) VALUES (?, ?, ?, ?, ?)";
+
+        try {
+        	
+            c = DBConnection.getInstance().startConnection();
+
+            ps = c.prepareStatement(query);
+    
+            ps.setString(1, cliente.getNome());
+            ps.setString(2, cliente.getCognome());
+            ps.setString(3, cliente.getEmail());
+            ps.setString(4, cliente.getPassword());
+            ps.setDate(5, java.sql.Date.valueOf(cliente.getData_nascita()));
+
+            int result = ps.executeUpdate();
+            return result > 0; // Ritorna true se l'inserimento è riuscito
+
+        } catch (SQLException e) {
+            System.err.println("Errore durante la registrazione: " + e.getMessage());
+
+        } finally {
+            DBConnection.getInstance().closeConnection(c);
+        }
+        return false;
+	}
+
+}
---
+++ src/it/unipv/posw/Model/Persistence/DAO/Interface/IClienteDAO.java
@@ -0,0 +1,12 @@
+package it.unipv.posw.Model.Persistence.DAO.Interface;
+
+import it.unipv.posw.Model.Cliente;
+
+/**
+* @author rkomi-dev
+*/
+
+public interface IClienteDAO {
+	
+	boolean salvaCliente(Cliente cliente);
+}
