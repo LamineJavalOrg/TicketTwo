d978a5b0f4d790834cbf93717c992ded375202ba
Mirko <mirkohtcz@gmail.com>
2026-06-08 10:07:33 +0200

    Creazione HomeView e interface IView per switchare le varie view (Costruzione)

---
+++ src/it/unipv/posw/view/HomeView.java
@@ -0,0 +1,28 @@
+package it.unipv.posw.view;
+
+import javafx.scene.Node;
+import javafx.scene.layout.VBox;
+
+public class HomeView extends VBox implements IView {
+	
+   
+    public HomeView() {
+    	
+    
+
+        this.getStylesheets().add(getClass().getResource("/css/home-css.css").toExternalForm());
+        this.getStyleClass().add("background");
+
+    }
+
+	@Override
+	public Node getNodo() {
+		// TODO Auto-generated method stub
+		return this;
+	}
+
+	
+
+    
+    
+}
---
+++ src/it/unipv/posw/view/IView.java
@@ -0,0 +1,11 @@
+package it.unipv.posw.view;
+
+import java.util.prefs.NodeChangeEvent;
+
+import javafx.scene.Node;
+
+public interface IView {
+	
+	Node getNodo();
+
+}
