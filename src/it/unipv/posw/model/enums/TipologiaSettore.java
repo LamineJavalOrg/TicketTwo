1ed9d70f7b4a10297db34232f6701269d12f662e
Pelle <gabriele.pellegatta01@universitadipavia.it>
2026-06-11 16:57:26 +0200

    Modifiche minori classe TipologiaSettore e css (Costruzione)

--- src/it/unipv/posw/model/enums/TipologiaSettore.java
+++ src/it/unipv/posw/model/enums/TipologiaSettore.java
@@ -4,5 +4,19 @@ package it.unipv.posw.model.enums;
  * @author gpelle
  */
 public enum TipologiaSettore {
-	PLATEA, PARTERRE, TRIBUNA, CURVA
+	PLATEA(false), 
+	PARTERRE(true), 
+	TRIBUNA(false),
+	CURVA(false);
+
+	
+	private final boolean soloNonNumerato;
+	
+	TipologiaSettore(boolean soloNonNumerato) {
+		this.soloNonNumerato = soloNonNumerato;
+	}
+	
+	public boolean isSoloNonNumerato() {
+		return soloNonNumerato;
+	}
 }
