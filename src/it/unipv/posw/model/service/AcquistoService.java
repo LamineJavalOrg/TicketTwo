c5d0e4b411958c690efdf6b336e43b6d349e953b
Mirko <mirkohtcz@gmail.com>
2026-06-15 16:57:47 +0200

    implementazione classi AcquistoService e QRService (Costruzione)

---
+++ src/it/unipv/posw/model/service/AcquistoService.java
@@ -0,0 +1,44 @@
+package it.unipv.posw.model.service;
+
+import it.unipv.posw.model.entities.Biglietto;
+import it.unipv.posw.model.entities.Carrello;
+import it.unipv.posw.model.entities.SessioneCliente;
+import it.unipv.posw.model.persistence.MYSQLDAOFactory;
+import it.unipv.posw.model.service.payment.IPagamento;
+
+/** 
+ * @author rkomi-dev
+ */
+
+public class AcquistoService {
+	
+	private QRService qrservice;
+	
+	public AcquistoService() {
+	}
+	
+	public void Acquista(IPagamento metodo) throws Exception {
+		
+		boolean esito = metodo.Paga(Carrello.getInstance().getTotale());
+		
+		qrservice = new QRService();
+		
+		qrservice.Genera();
+		SessioneCliente.getInstance().setQr_attuale(qrservice.getCodice());
+		
+		for(Biglietto b : Carrello.getInstance().getItems()) {
+			
+			String email = SessioneCliente.getInstance().getUtenteLoggato().getEmail();
+			String nome = SessioneCliente.getInstance().getUtenteLoggato().getNome();
+			String cognome = SessioneCliente.getInstance().getUtenteLoggato().getCognome();
+			String qr = SessioneCliente.getInstance().getQr_attuale();
+			
+			MYSQLDAOFactory.getInstance().getBigliettoDAO().updatePostAcquisto(b.getId_biglietto(), email, nome + " " + cognome, qr);
+		}
+		
+		if(!esito) {
+			throw new Exception();
+		}
+		
+	}
+}
---
+++ src/it/unipv/posw/model/service/QRService.java
@@ -0,0 +1,30 @@
+package it.unipv.posw.model.service;
+
+/** 
+ * @author rkomi-dev
+ */
+
+public class QRService {
+	
+	private String codice; 
+	public final int QRDIGITS = 8;
+	
+	public QRService() {
+
+	}
+
+	public void Genera() {
+		
+		codice = "QR";
+		for(int i = 0; i < QRDIGITS; i++) {
+			codice += (int)(9 * Math.random());
+		}
+	}
+
+	public String getCodice() {
+		return codice;
+	}
+	
+	
+
+}
